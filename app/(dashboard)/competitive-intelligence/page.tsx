384cbfbf74ae72b55601292e5635ced54872eb78
Aliky Anas <anasalk37@gmail.com>
2025-11-27 15:18:43 +0100

    updates

--- app/(dashboard)/competitive-intelligence/page.tsx
+++ app/(dashboard)/competitive-intelligence/page.tsx
@@ -33,6 +33,15 @@ const networks = [
 const page = () => {
   return (
     <div className="@container/main flex flex-col">
+      <div>
+        <h2 className="text-4xl sm:text-5xl md:text-6xl my-3 font-extrabold tracking-tight text-gray-900 dark:text-white">
+          Veille Concurrentielle
+        </h2>
+        {/* <p className="mt-1 text-sm sm:text-base text-muted-foreground max-w-3xl">
+          Générez et téléchargez des rapports détaillés sur vos performances sur
+          les re9seaux sociaux, les insights d&apos;audience et plus encore.
+        </p> */}
+      </div>
       <div className="flex flex-col gap-5 container mx-auto">
         <CompetitiveIntelligenceTable
           networks={networks}
